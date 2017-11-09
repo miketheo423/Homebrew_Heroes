b79e096250a71ce247684363c39deb6e10a37493
Ryan <ryan.d.elliott@gmail.com>
2017-11-09 14:30:00 -0700

    Update seed file

--- back-end/db/seed.js
+++ back-end/db/seed.js
@@ -35,17 +35,8 @@ const seedBeers1 = [
 		abv: '7.2',
 		ibus: '24',
 		description: 'Hop Back Salamanzar Chill Haze Chill Haze Crystal malt, Racking. Stick, Oxidized Kilderkin Biere de garde. Nebuchadnezzar Mead Flute Double Magnum Amber, ” Draugh Wort Wort Chiller Mug.” Pub glass Bottle Conditioning Oxidized Ethanol or Ethyl Alcohol. Brew Kettle Grist Ale Aroma Hops Bottom-fermenting Yeast Ale Noble Hops Aroma Hops Brewhouse, Hydrometer.',
-		recipe: `
-			For 5 gallons (19 L)\n
-			4.5 lb. (2 kg) German or other light or amber malt extract syrup\n
-			3.3. lb. (1.5 kg) plain dark malt extract syrup\n
-			1 lb. (450 g) black malt\n
-			1.5 oz. (42 g) Northern Brewer hops (boiling), 13 HBU\n
-			1 oz. (28 g) Tettnang or Santiam hops (aroma)\n
-			0.25 tsp. (1 g) powdered Irish moss\n
-			American Ale yeast or White Labs Cry Havoc yeast\n
-			0.75 cup (175 mL) corn sugar or 1.25 cup (300 mL) dried malt extract (for bottling)`,
-			userId: ''
+		recipe: `For 5 gallons (19 L)\n4.5 lb. (2 kg) German or other light or amber malt extract syrup\n3.3. lb. (1.5 kg) plain dark malt extract syrup\n1 lb. (450 g) black malt\n1.5 oz. (42 g) Northern Brewer hops (boiling), 13 HBU\n1 oz. (28 g) Tettnang or Santiam hops (aroma)\n0.25 tsp. (1 g) powdered Irish moss\nAmerican Ale yeast or White Labs Cry Havoc yeast\n0.75 cup (175 mL) corn sugar or 1.25 cup (300 mL) dried malt extract (for bottling)`,
+		userId: ''
 	},
 	{
 		photoUrl: 'http://www.iconarchive.com/download/i97927/flat-icons.com/flat/Beer.ico', 
@@ -54,17 +45,8 @@ const seedBeers1 = [
 		abv: '4.5',
 		ibus: '20',
 		description: 'Hop Back Salamanzar Chill Haze Chill Haze Crystal malt, Racking. Stick, Oxidized Kilderkin Biere de garde. Nebuchadnezzar Mead Flute Double Magnum Amber, ” Draugh Wort Wort Chiller Mug.” Pub glass Bottle Conditioning Oxidized Ethanol or Ethyl Alcohol. Brew Kettle Grist Ale Aroma Hops Bottom-fermenting Yeast Ale Noble Hops Aroma Hops Brewhouse, Hydrometer.',
-		recipe: `
-			For 5 gallons (19 L)\n
-			4.5 lb. (2 kg) German or other light or amber malt extract syrup\n
-			3.3. lb. (1.5 kg) plain dark malt extract syrup\n
-			1 lb. (450 g) black malt\n
-			1.5 oz. (42 g) Northern Brewer hops (boiling), 13 HBU\n
-			1 oz. (28 g) Tettnang or Santiam hops (aroma)\n
-			0.25 tsp. (1 g) powdered Irish moss\n
-			American Ale yeast or White Labs Cry Havoc yeast\n
-			0.75 cup (175 mL) corn sugar or 1.25 cup (300 mL) dried malt extract (for bottling)`,
-			userId: ''
+		recipe: `For 5 gallons (19 L)\n4.5 lb. (2 kg) German or other light or amber malt extract syrup\n3.3. lb. (1.5 kg) plain dark malt extract syrup\n1 lb. (450 g) black malt\n1.5 oz. (42 g) Northern Brewer hops (boiling), 13 HBU\n1 oz. (28 g) Tettnang or Santiam hops (aroma)\n0.25 tsp. (1 g) powdered Irish moss\nAmerican Ale yeast or White Labs Cry Havoc yeast\n0.75 cup (175 mL) corn sugar or 1.25 cup (300 mL) dried malt extract (for bottling)`,
+		userId: ''
 	},
 	{
 		photoUrl: 'http://www.iconarchive.com/download/i97927/flat-icons.com/flat/Beer.ico', 
@@ -73,17 +55,8 @@ const seedBeers1 = [
 		abv: '6.2',
 		ibus: '99',
 		description: 'Hop Back Salamanzar Chill Haze Chill Haze Crystal malt, Racking. Stick, Oxidized Kilderkin Biere de garde. Nebuchadnezzar Mead Flute Double Magnum Amber, ” Draugh Wort Wort Chiller Mug.” Pub glass Bottle Conditioning Oxidized Ethanol or Ethyl Alcohol. Brew Kettle Grist Ale Aroma Hops Bottom-fermenting Yeast Ale Noble Hops Aroma Hops Brewhouse, Hydrometer.',
-		recipe: `
-			For 5 gallons (19 L)\n
-			4.5 lb. (2 kg) German or other light or amber malt extract syrup\n
-			3.3. lb. (1.5 kg) plain dark malt extract syrup\n
-			1 lb. (450 g) black malt\n
-			1.5 oz. (42 g) Northern Brewer hops (boiling), 13 HBU\n
-			1 oz. (28 g) Tettnang or Santiam hops (aroma)\n
-			0.25 tsp. (1 g) powdered Irish moss\n
-			American Ale yeast or White Labs Cry Havoc yeast\n
-			0.75 cup (175 mL) corn sugar or 1.25 cup (300 mL) dried malt extract (for bottling)`,
-			userId: ''
+		recipe: `For 5 gallons (19 L)\n4.5 lb. (2 kg) German or other light or amber malt extract syrup\n3.3. lb. (1.5 kg) plain dark malt extract syrup\n1 lb. (450 g) black malt\n1.5 oz. (42 g) Northern Brewer hops (boiling), 13 HBU\n1 oz. (28 g) Tettnang or Santiam hops (aroma)\n0.25 tsp. (1 g) powdered Irish moss\nAmerican Ale yeast or White Labs Cry Havoc yeast\n0.75 cup (175 mL) corn sugar or 1.25 cup (300 mL) dried malt extract (for bottling)`,
+		userId: ''
 	}
 ];
 
@@ -95,17 +68,8 @@ const seedBeers2 = [
 		abv: '7.2',
 		ibus: '92',
 		description: 'Hop Back Salamanzar Chill Haze Chill Haze Crystal malt, Racking. Stick, Oxidized Kilderkin Biere de garde. Nebuchadnezzar Mead Flute Double Magnum Amber, ” Draugh Wort Wort Chiller Mug.” Pub glass Bottle Conditioning Oxidized Ethanol or Ethyl Alcohol. Brew Kettle Grist Ale Aroma Hops Bottom-fermenting Yeast Ale Noble Hops Aroma Hops Brewhouse, Hydrometer.',
-		recipe: `
-			For 5 gallons (19 L)\n
-			4.5 lb. (2 kg) German or other light or amber malt extract syrup\n
-			3.3. lb. (1.5 kg) plain dark malt extract syrup\n
-			1 lb. (450 g) black malt\n
-			1.5 oz. (42 g) Northern Brewer hops (boiling), 13 HBU\n
-			1 oz. (28 g) Tettnang or Santiam hops (aroma)\n
-			0.25 tsp. (1 g) powdered Irish moss\n
-			American Ale yeast or White Labs Cry Havoc yeast\n
-			0.75 cup (175 mL) corn sugar or 1.25 cup (300 mL) dried malt extract (for bottling)`,
-			userId: ''
+		recipe: `For 5 gallons (19 L)\n4.5 lb. (2 kg) German or other light or amber malt extract syrup\n3.3. lb. (1.5 kg) plain dark malt extract syrup\n1 lb. (450 g) black malt\n1.5 oz. (42 g) Northern Brewer hops (boiling), 13 HBU\n1 oz. (28 g) Tettnang or Santiam hops (aroma)\n0.25 tsp. (1 g) powdered Irish moss\nAmerican Ale yeast or White Labs Cry Havoc yeast\n0.75 cup (175 mL) corn sugar or 1.25 cup (300 mL) dried malt extract (for bottling)`,
+		userId: ''
 	},
 	{
 		photoUrl: 'http://www.iconarchive.com/download/i97927/flat-icons.com/flat/Beer.ico', 
@@ -114,17 +78,8 @@ const seedBeers2 = [
 		abv: '4.5',
 		ibus: '20',
 		description: 'Hop Back Salamanzar Chill Haze Chill Haze Crystal malt, Racking. Stick, Oxidized Kilderkin Biere de garde. Nebuchadnezzar Mead Flute Double Magnum Amber, ” Draugh Wort Wort Chiller Mug.” Pub glass Bottle Conditioning Oxidized Ethanol or Ethyl Alcohol. Brew Kettle Grist Ale Aroma Hops Bottom-fermenting Yeast Ale Noble Hops Aroma Hops Brewhouse, Hydrometer.',
-		recipe: `
-			For 5 gallons (19 L)\n
-			4.5 lb. (2 kg) German or other light or amber malt extract syrup\n
-			3.3. lb. (1.5 kg) plain dark malt extract syrup\n
-			1 lb. (450 g) black malt\n
-			1.5 oz. (42 g) Northern Brewer hops (boiling), 13 HBU\n
-			1 oz. (28 g) Tettnang or Santiam hops (aroma)\n
-			0.25 tsp. (1 g) powdered Irish moss\n
-			American Ale yeast or White Labs Cry Havoc yeast\n
-			0.75 cup (175 mL) corn sugar or 1.25 cup (300 mL) dried malt extract (for bottling)`,
-			userId: ''
+		recipe: `For 5 gallons (19 L)\n4.5 lb. (2 kg) German or other light or amber malt extract syrup\n3.3. lb. (1.5 kg) plain dark malt extract syrup\n1 lb. (450 g) black malt\n1.5 oz. (42 g) Northern Brewer hops (boiling), 13 HBU\n1 oz. (28 g) Tettnang or Santiam hops (aroma)\n0.25 tsp. (1 g) powdered Irish moss\nAmerican Ale yeast or White Labs Cry Havoc yeast\n0.75 cup (175 mL) corn sugar or 1.25 cup (300 mL) dried malt extract (for bottling)`,
+		userId: ''
 	},
 	{
 		photoUrl: 'http://www.iconarchive.com/download/i97927/flat-icons.com/flat/Beer.ico', 
@@ -133,17 +88,8 @@ const seedBeers2 = [
 		abv: '6.2',
 		ibus: '14',
 		description: 'Hop Back Salamanzar Chill Haze Chill Haze Crystal malt, Racking. Stick, Oxidized Kilderkin Biere de garde. Nebuchadnezzar Mead Flute Double Magnum Amber, ” Draugh Wort Wort Chiller Mug.” Pub glass Bottle Conditioning Oxidized Ethanol or Ethyl Alcohol. Brew Kettle Grist Ale Aroma Hops Bottom-fermenting Yeast Ale Noble Hops Aroma Hops Brewhouse, Hydrometer.',
-		recipe: `
-			For 5 gallons (19 L)\n
-			4.5 lb. (2 kg) German or other light or amber malt extract syrup\n
-			3.3. lb. (1.5 kg) plain dark malt extract syrup\n
-			1 lb. (450 g) black malt\n
-			1.5 oz. (42 g) Northern Brewer hops (boiling), 13 HBU\n
-			1 oz. (28 g) Tettnang or Santiam hops (aroma)\n
-			0.25 tsp. (1 g) powdered Irish moss\n
-			American Ale yeast or White Labs Cry Havoc yeast\n
-			0.75 cup (175 mL) corn sugar or 1.25 cup (300 mL) dried malt extract (for bottling)`,
-			userId: ''
+		recipe: `For 5 gallons (19 L)\n4.5 lb. (2 kg) German or other light or amber malt extract syrup\n3.3. lb. (1.5 kg) plain dark malt extract syrup\n1 lb. (450 g) black malt\n1.5 oz. (42 g) Northern Brewer hops (boiling), 13 HBU\n1 oz. (28 g) Tettnang or Santiam hops (aroma)\n0.25 tsp. (1 g) powdered Irish moss\nAmerican Ale yeast or White Labs Cry Havoc yeast\n0.75 cup (175 mL) corn sugar or 1.25 cup (300 mL) dried malt extract (for bottling)`,
+		userId: ''
 	}
 ];
 
@@ -160,6 +106,7 @@ function createUser(userInfo, beers){
 		});
 }
 
+/* Seed the db */
 createUser(seedUser1, seedBeers1);
 createUser(seedUser2, seedBeers2);
 createUser(seedUser3, []);
