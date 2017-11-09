a9695f83057e869d3aa3bbc99f1e6eae09a314cc
Ryan <ryan.d.elliott@gmail.com>
2017-11-08 22:37:54 -0700

    Add user and beer routes to app routing module

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -5,8 +5,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { BeerFeedPageComponent } from './beer-feed-page/beer-feed-page.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+
 import { UsersShowComponent } from './users/users-show/users-show.component';
+import { UsersEditComponent } from './users/users-edit/users-edit.component';
+
 import { BeersShowComponent } from './beers/beers-show/beers-show.component';
+import { BeersEditComponent } from './beers/beers-edit/beers-edit.component';
+import { BeersNewComponent } from './beers/beers-new/beers-new.component';
 
 const routes: Routes = [
 	{
@@ -16,7 +21,45 @@ const routes: Routes = [
 	{
 		path: 'welcome',
 		component: LandingPageComponent
-	}
+	},
+	/* beers and user routes could be broken out into module routes */
+	{
+		path: 'beers',
+		children: [
+			{
+				path: '',
+				component: BeerFeedPageComponent
+			},
+			{
+				path: 'new',
+				component: BeersNewComponent
+			},
+			{
+				path: 'edit/:id',
+				component: BeersEditComponent
+			},
+			{
+				path: ':id',
+				component: BeersShowComponent
+			}
+		]
+	},
+	{
+		path: 'brewer',
+		children: [
+			{
+				path: 'edit', // :id not necessary becuase user will only be able to edit their own profile
+				component: UsersEditComponent
+			},
+			{
+				path: ':userName', // individual user profile homebrewheroes.com/brewer/mrbrew
+				component: UsersShowComponent
+			}
+		]
+	},
+
+	// all others redirect to beer feed
+	{ path: '**', redirectTo: '' }
 ]
 
 @NgModule({
