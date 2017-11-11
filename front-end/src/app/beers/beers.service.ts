726a59574c79e7750761e2f857a4f97161c6e0f6
Ryan <ryan.d.elliott@gmail.com>
2017-11-11 16:16:45 -0700

    Create beer service methods get/post/put/delete

--- front-end/src/app/beers/beers.service.ts
+++ front-end/src/app/beers/beers.service.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class BeersService {
+	baseUrl: string = '';
 
-  constructor() { }
+  constructor(private http: Http) { 
+  	if (isDevMode()) this.baseUrl = 'http://localhost:3000';
+  }
+
+  /* get beer profile from api */
+  getBeerProfileInfo(beerId){
+  	return this.http.get(`${this.baseUrl}/api/beers/${beerId}`);
+  }
+
+  /* create a new beer with form information */
+  createBeer(newBeer){
+  	return this.http.post(`${this.baseUrl}/api/beers`, newBeer);
+  }
+
+  /* edit a beer with form information */
+  editBeer(beerId, editedBeer){
+  	return this.http.put(`${this.baseUrl}/api/beers/${beerId}`, editedBeer);
+  }
+
+  /* delete a beer */
+  deleteBeer(beerId){
+  	return this.http.delete(`${this.baseUrl}/api/beers/${beerId}`);
+  }
 
 }
