befa074042123c490ccf9cd37709c0af9a4dbff1
Ryan <ryan.d.elliott@gmail.com>
2017-11-11 16:17:14 -0700

    Add service to providers in beers.module

--- front-end/src/app/beers/beers.module.ts
+++ front-end/src/app/beers/beers.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { BeersNewComponent } from './beers-new/beers-new.component';
 import { BeersShowComponent } from './beers-show/beers-show.component';
 import { BeersEditComponent } from './beers-edit/beers-edit.component';
+import { BeersService } from './beers.service';
+
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [BeersNewComponent, BeersShowComponent, BeersEditComponent]
+  declarations: [BeersNewComponent, BeersShowComponent, BeersEditComponent],
+  providers: [BeersService]
 })
 export class BeersModule { }
