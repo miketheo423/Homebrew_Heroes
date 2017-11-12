aef32c19c007d9c460ab7d67fdd126f790d20790
Ryan <ryan.d.elliott@gmail.com>
2017-11-12 15:55:15 -0700

    Create header component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { BeerFeedPageComponent } from './beer-feed-page/beer-feed-page.component
 import { BeerCardComponent } from './beer-card/beer-card.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { FooterComponent } from './footer/footer.component';
     LoginModalComponent,
     BeerFeedPageComponent,
     BeerCardComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
