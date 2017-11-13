9ed8ed76fa3abdea1b985ffdadb71416d5f4330d
miketheo423 <miketheo423@gmail.com>
2017-11-12 18:41:40 -0700

    Adds intial styling to beer show page

--- front-end/src/app/beers/beers-show/beers-show.component.ts
+++ front-end/src/app/beers/beers-show/beers-show.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BeersService } from '../beers.service';
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-beers-show',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BeersShowComponent implements OnInit {
 
-  constructor() { }
+	oneBeer;
+
+  constructor(
+  	private route: ActivatedRoute,
+  	private beersService: BeersService
+  	) { }
 
   ngOnInit() {
+  	this.route.params.forEach( param => {
+  		this.beersService.getBeerProfileInfo(param.id)
+  		.subscribe(response => {
+  			console.log(response.json());
+  			this.oneBeer = response.json();
+  		});
+  	});
   }
 
 }
