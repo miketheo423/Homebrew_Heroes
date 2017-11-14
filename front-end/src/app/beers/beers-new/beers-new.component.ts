65a6f18824916034cdccc097c343411e7ca67142
miketheo423 <miketheo423@gmail.com>
2017-11-14 15:12:13 -0700

    More bug fixes

--- front-end/src/app/beers/beers-new/beers-new.component.ts
+++ front-end/src/app/beers/beers-new/beers-new.component.ts
@@ -18,33 +18,13 @@ export class BeersNewComponent implements OnInit {
 		recipe: ''
 	}
 
-	darkBeers = ['Stout', 'Porter', 'Schwarzbier']
-	wheatBeers = ['Hefeweizen', 'American Wheat Beer', 'Witbier']
-	brownBeers = ['Brown Ale', 'Scottish Ale']
-	IPAS = ['American IPA', 'Double IPA', 'Triple IPA', 'English IPA', 'Amber Ale']
-	sours = ['Sour', 'Gose']
-
   constructor(private beersService : BeersService, private router: Router) { }
 
   ngOnInit() {
   }
 
  	saveBeer(){
- 		// Conditionals to determine what photo is assigned to new beer
- 		if (this.darkBeers.includes(this.newBeer.style)) {
- 			this.newBeer.photoUrl = "http://hopworksbeer.com/wp-content/uploads/2015/07/HUB_Site_Pint_STOUT.jpg"
- 		} else if (this.wheatBeers.includes(this.newBeer.style)) {
- 			this.newBeer.photoUrl = "http://cervezacasera.com.mx/wp-content/uploads/2016/02/Kit_Todo_Grano_T_54c1743c49f7e.jpg"
- 		} else if (this.brownBeers.includes(this.newBeer.style)) {
- 			this.newBeer.photoUrl = "https://www.ritebrew.com/v/vspfiles/photos/851316-2.jpg"
- 		} else if (this.IPAS.includes(this.newBeer.style)) {
- 			this.newBeer.photoUrl = "https://www.mrbeer.com/media/catalog/product/cache/2/thumbnail/9df78eab33525d08d6e5fb8d27136e95/b/e/beer-amber-pintulw_4.jpg"
- 		} else if (this.sours.includes(this.newBeer.style)) {
- 			this.newBeer.photoUrl = "https://cdnimg.webstaurantstore.com/uploads/buying_guide/2017/2/berliner-weisse.jpg"
- 		} else if (this.newBeer.style === 'Pilsner') {
- 			this.newBeer.photoUrl = "https://www.displayfakefoods.com/store/pc/catalog/2359-lg.jpg"
- 		}
-
+ 		this.newBeer.photoUrl = this.checkStyle(this.newBeer.style)
  		this.beersService.createBeer(this.newBeer)
  			.subscribe(response => {
  				console.log(response.json());
@@ -53,6 +33,32 @@ export class BeersNewComponent implements OnInit {
  					// window.location.href = '/beers';
  				}
  			})
+ 	}
+
+ 	checkStyle(style) {
+
+ 		let darkBeers = ['Stout', 'Porter', 'Schwarzbier']
+ 		let wheatBeers = ['Hefeweizen', 'American Wheat Beer', 'Witbier']
+ 		let brownBeers = ['Brown Ale', 'Scottish Ale']
+ 		let IPAS = ['American IPA', 'Double IPA', 'Triple IPA', 'English IPA', 'Amber Ale']
+ 		let sours = ['Sour', 'Gose']
+ 		let photoUrl = '';
+
+		// Conditionals to determine what photo is assigned to new beer
+ 		if (darkBeers.includes(style)) {
+ 			photoUrl = "http://hopworksbeer.com/wp-content/uploads/2015/07/HUB_Site_Pint_STOUT.jpg"
+ 		} else if (wheatBeers.includes(style)) {
+ 			photoUrl = "http://cervezacasera.com.mx/wp-content/uploads/2016/02/Kit_Todo_Grano_T_54c1743c49f7e.jpg"
+ 		} else if (brownBeers.includes(style)) {
+ 			photoUrl = "https://www.ritebrew.com/v/vspfiles/photos/851316-2.jpg"
+ 		} else if (IPAS.includes(style)) {
+ 			photoUrl = "https://www.mrbeer.com/media/catalog/product/cache/2/thumbnail/9df78eab33525d08d6e5fb8d27136e95/b/e/beer-amber-pintulw_4.jpg"
+ 		} else if (sours.includes(style)) {
+ 			photoUrl = "https://cdnimg.webstaurantstore.com/uploads/buying_guide/2017/2/berliner-weisse.jpg"
+ 		} else {
+ 			photoUrl = "https://www.displayfakefoods.com/store/pc/catalog/2359-lg.jpg"
+ 		}
+ 		return photoUrl;
  	}
 
 }
